1e2c79e866d8efb3a4a22e0e66221491aeb390af
Alan Taylor <alancarltaylor@gmail.com>
2017-07-28 14:09:09 -0600

    added scroll event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+
+  boolChangeClass: boolean;
+  constructor(@Inject(DOCUMENT) private document: Document){
+    document.body.style.overflowX = "hidden";
+  }
   title = 'app';
+
+  @HostListener("window:scroll", [])
+    onWindowScroll() {
+     //we'll do some stuff here when the window is scrolled
+     let number = this.document.body.scrollTop;
+     if ( number >= 100) {
+        this.boolChangeClass = true;
+        console.log('Scrolled below header.');
+    } else {
+        this.boolChangeClass = false;
+        console.log('Header is in view.');
+   }
+    }
+
+  ngOnInit(){
+
+}
 }
