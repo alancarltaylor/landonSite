4c8c72f9c6a0fab262972f63b0f53d71a3e53acc
Alan Taylor <alancarltaylor@gmail.com>
2017-09-21 22:06:03 -0600

    not there yet with the scrolling

--- src/app/welcome-banner/welcome-banner.component.ts
+++ src/app/welcome-banner/welcome-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-welcome-banner',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome-banner.component.css']
 })
 export class WelcomeBannerComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit() {
+  }
+
+  scroll(el, pos){
+    // console.log("el: ", el);
+    var y = el.scrollTop;
+    y += Math.round( ( pos - y ) * 0.3 );
+    if (Math.abs(y-pos) <= 2)
+    {
+        el.scrollTop = pos;
+        return;
+    }
+    el.scrollTop = y;
+    setTimeout(scroll, 40, el, pos);
+    el.scrollIntoView();
   }
 
 }
